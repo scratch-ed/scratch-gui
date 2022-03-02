8290e593df4146ab32fbc278eddf73e32a687b18
Robbe De Proft <rdeproft12@gmail.com>
2022-03-02 12:06:38 +0100

    Add formattedmessage for trail slider

--- src/components/debugger-tab/debugger-tab.jsx
+++ src/components/debugger-tab/debugger-tab.jsx
@@ -5,6 +5,7 @@ import Box from '../box/box.jsx';
 import SliderComponent from '../slider/slider.jsx';
 
 import styles from './debugger-tab.css';
+import {FormattedMessage} from 'react-intl';
 
 const DebuggerTabComponent = function (props) {
     const {
@@ -19,7 +20,13 @@ const DebuggerTabComponent = function (props) {
     return (
         <Box className={styles.debuggerTab}>
             <label>
-                <span>{'Trail length: '}</span>
+                <span>
+                    <FormattedMessage
+                        defaultMessage="Trail length"
+                        description="Trail length slider label"
+                        id="gui.debuggerTab.trailLength"
+                    />
+                </span>
                 <SliderComponent
                     max={MAX_TRAIL_LENGTH}
                     onChange={onTrailChange}
