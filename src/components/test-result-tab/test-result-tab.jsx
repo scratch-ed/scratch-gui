ce09a18b2669b8c6d33ad8f740496bb6565506fa
Iwijn <iwijn.voeten@gmail.com>
2022-12-04 16:57:31 +0100

    basic tree structure is visible, added passed, failed, maybe images

--- src/components/test-result-tab/test-result-tab.jsx
+++ src/components/test-result-tab/test-result-tab.jsx
@@ -12,7 +12,7 @@ const TestResultTabComponent = function (props) {
     } = props;
 
     return (
-        <Box className={styles.debuggerTab}>
+        <Box className={styles.testResultTab}>
             <label>
                 <span>
                     <FormattedMessage
@@ -22,11 +22,11 @@ const TestResultTabComponent = function (props) {
                     />
                 </span>
                 <br />
-                <span>
-                    { // TODO: make test result component
-                        getTestResults().map((result, i) => <div key={i}>{result}<br /></div>)
-                    }
-                </span>
+                <FeedbackTreeComponent
+                    feedbackTree={Object.values(getTestResults())[0] ?
+                        Object.values(getTestResults())[0] : {value: '', children: []}}
+                    depth={0}
+                />
             </label>
         </Box>
     );
@@ -34,6 +34,27 @@ const TestResultTabComponent = function (props) {
 
 TestResultTabComponent.propTypes = {
     getTestResults: PropTypes.func.isRequired
+};
+
+const FeedbackTreeComponent = function (props) {
+    const {
+        feedbackTree,
+        depth
+    } = props;
+
+    return (
+        <div>
+            <a>{(feedbackTree.groupPassed ? 'Passed: ' : 'Failed: ') + feedbackTree.value}</a>
+            <div className={styles.feedbackTree}>
+                {feedbackTree.children.map((child => FeedbackTreeComponent({feedbackTree: child, depth: (depth + 1)})))}
+            </div>
+        </div>
+    );
+};
+
+FeedbackTreeComponent.propTypes = {
+    feedbackTree: PropTypes.object.isRequired,
+    depth: PropTypes.number.isRequired
 };
 
 export default TestResultTabComponent;
