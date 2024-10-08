d1deffcf7fd2158e073cca6547f6fabbbafef095
Vital Wang <vital.wang@hotmail.com>
2024-10-08 12:11:24 +0200

    fix: flickering when running tests

--- src/containers/test-tab.jsx
+++ src/containers/test-tab.jsx
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {runInVM} from 'itch';
+import {runInVM, snapshotFromVm} from 'itch';
 import TestResultComponent from '../components/test-results/test-results.jsx';
 import {connect} from 'react-redux';
 import {VM} from 'scratch-vm';
 
-const TestTab = ({vm, getTestResults, testCallback}) => {
+const TestTab = ({vm, clear, loadProject, saveProjectSb3, getTestResults, testCallback}) => {
     const [filesUploaded, setFilesUploaded] = useState(false);
     const [config, setConfig] = useState(null);
     const [template, setTemplate] = useState(null);
@@ -49,7 +49,11 @@ const TestTab = ({vm, getTestResults, testCallback}) => {
                     const filePath = webkitPath.substring(webkitPath.indexOf('/') + 1);
 
                     if (filePath === conf.template) {
-                        setTemplate(await new Response(files[i]).arrayBuffer());
+                        const currentSb3 = await new Response(await saveProjectSb3()).arrayBuffer();
+                        await loadProject(await new Response(files[i]).arrayBuffer());
+                        setTemplate(snapshotFromVm(vm));
+                        await clear();
+                        await loadProject(currentSb3);
                         if (found) return;
                         found = true;
                     }
@@ -82,8 +86,7 @@ const TestTab = ({vm, getTestResults, testCallback}) => {
                 await runInVM({
                     ...config,
                     template,
-                    callback,
-                    canvas: document.getElementById('scratch-stage')
+                    callback
                 }, vm);
             } catch (error) {
                 if (error.name === 'TypeError' && error.message === 'runtime.getSpriteTargetByName(...) is undefined') {
@@ -134,12 +137,18 @@ const TestTab = ({vm, getTestResults, testCallback}) => {
 };
 
 TestTab.propTypes = {
+    clear: PropTypes.func,
+    loadProject: PropTypes.func,
+    saveProjectSb3: PropTypes.func,
     getTestResults: PropTypes.func,
     testCallback: PropTypes.func,
     vm: PropTypes.instanceOf(VM).isRequired
 };
 
 const mapStateToProps = state => ({
+    clear: state.scratchGui.vm.clear.bind(state.scratchGui.vm),
+    loadProject: state.scratchGui.vm.loadProject.bind(state.scratchGui.vm),
+    saveProjectSb3: state.scratchGui.vm.saveProjectSb3.bind(state.scratchGui.vm),
     testCallback: state.scratchGui.vm.processTestFeedback.bind(state.scratchGui.vm),
     getTestResults: state.scratchGui.vm.getTestResults.bind(state.scratchGui.vm)
 });
