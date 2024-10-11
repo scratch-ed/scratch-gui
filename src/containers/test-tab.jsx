b6d7ddc652538eb8e1bd02c58b6cc9b4dd5330d4
Vital Wang <vital.wang@hotmail.com>
2024-10-11 23:49:36 +0200

    feat: add test to sb3

--- src/containers/test-tab.jsx
+++ src/containers/test-tab.jsx
@@ -5,79 +5,19 @@ import TestResultComponent from '../components/test-results/test-results.jsx';
 import {connect} from 'react-redux';
 import {VM} from 'scratch-vm';
 
-const TestTab = ({vm, clear, loadProject, saveProjectSb3, getTestResults, testCallback}) => {
-    const [filesUploaded, setFilesUploaded] = useState(false);
-    const [config, setConfig] = useState(null);
-    const [template, setTemplate] = useState(null);
-    const [scriptText, setScriptText] = useState(null);
+const TestTab = ({vm, getTestResults, testCallback}) => {
+    const config = vm.testConfig;
+    const template = vm.testTemplate;
+    const testPlan = vm.testPlan;
     const [testFailed, setTestFailed] = useState(false);
 
-    let failureMessage = null;
-    if (filesUploaded) {
-        if (config === null) {
-            failureMessage = 'Config file not found';
-        } else if (template === null) {
-            failureMessage = 'Template project not found';
-        } else if (scriptText === null) {
-            failureMessage = 'Testplan not found';
-        }
-    }
-
-    const handleFileChange = async e => {
-        if (testFailed) {
-            setTestFailed(false);
-        }
-
-        if (e.target.files) {
-            setFilesUploaded(true);
-            const files = e.target.files;
-            let conf;
-            for (let i = 0; i < files.length; i++) {
-                if (files[i].name === 'config.json') {
-                    conf = await new Response(files[i]).json();
-                    setConfig(conf);
-                    break;
-                }
-            }
-
-            if (conf) {
-                let found = false;
-                for (let i = 0; i < files.length; i++) {
-                    // webkitRelativePath contains the folder name that was uploaded
-                    const webkitPath = files[i].webkitRelativePath;
-                    // strip first part of path to get rid of folder name
-                    const filePath = webkitPath.substring(webkitPath.indexOf('/') + 1);
-
-                    if (filePath === conf.template) {
-                        const currentSb3 = await new Response(await saveProjectSb3()).arrayBuffer();
-                        await loadProject(await new Response(files[i]).arrayBuffer());
-                        setTemplate(snapshotFromVm(vm));
-                        await clear();
-                        await loadProject(currentSb3);
-                        if (found) return;
-                        found = true;
-                    }
-                    if (filePath === conf.testplan) {
-                        setScriptText(await new Response(files[i]).text());
-                        if (found) return;
-                        found = true;
-                    }
-                }
-            }
-        }
-    };
-
     const handleUpload = async () => {
-        if (testFailed) {
-            setTestFailed(false);
-        }
-
         if (config) {
             const script = document.createElement('script');
             script.className = 'test-script';
             script.type = 'text/javascript';
             script.async = false;
-            script.innerHTML = scriptText;
+            script.innerHTML = testPlan;
             document.head.appendChild(script);
 
             const callback = testCallback;
@@ -102,53 +42,26 @@ const TestTab = ({vm, clear, loadProject, saveProjectSb3, getTestResults, testCa
 
     return (
         <div>
-            <h1>{'Runner Environment'}</h1>
+            <button
+                onClick={handleUpload}
+                className="submit"
+            >{'Run tests'}</button>
 
-            <div className="run-form">
-                <input
-                    type="file"
-                    id="submission"
-                    webkitdirectory=""
-                    directory=""
-                    onChange={handleFileChange}
-                />
-            </div>
-            {(filesUploaded && failureMessage === null) && (
-                <button
-                    onClick={handleUpload}
-                    className="submit"
-                >{'Upload test'}</button>
-            )}
-            <span>{failureMessage}</span>
-
-            <canvas id="scratch-stage" />
-
-            <h2 id="out">{'Output'}</h2>
             {testFailed && (
                 <span>{'Sprites in submission don\'t match sprites in template'}</span>
             )}
             <div><TestResultComponent testResults={getTestResults()} /></div>
-            <div
-                id="output"
-                style={{maxHeight: '200px', overflowY: 'auto'}}
-            />
         </div>
     );
 };
 
 TestTab.propTypes = {
-    clear: PropTypes.func,
-    loadProject: PropTypes.func,
-    saveProjectSb3: PropTypes.func,
     getTestResults: PropTypes.func,
     testCallback: PropTypes.func,
     vm: PropTypes.instanceOf(VM).isRequired
 };
 
 const mapStateToProps = state => ({
-    clear: state.scratchGui.vm.clear.bind(state.scratchGui.vm),
-    loadProject: state.scratchGui.vm.loadProject.bind(state.scratchGui.vm),
-    saveProjectSb3: state.scratchGui.vm.saveProjectSb3.bind(state.scratchGui.vm),
     testCallback: state.scratchGui.vm.processTestFeedback.bind(state.scratchGui.vm),
     getTestResults: state.scratchGui.vm.getTestResults.bind(state.scratchGui.vm)
 });
