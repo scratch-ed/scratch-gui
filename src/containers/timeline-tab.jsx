52518ca4a646f9476f1ad95266b19e3f36c05ef3
emmavdwa <emmavdwa.vandewalle@ugent.be>
2025-10-04 16:53:02 +0200

    feat: zoom in/out buttons placed

--- src/containers/timeline-tab.jsx
+++ src/containers/timeline-tab.jsx
@@ -2,12 +2,29 @@ import React from 'react';
 
 import Box from '../components/box/box.jsx';
 import Timeline from '../components/timeline/timeline.jsx';
+import zoomIn from '../components/timeline/zoom-in.png';
+import zoomOut from '../components/timeline/zoom-out.png';
 
 import styles from '../components/test-results/test-results.css';
+import timelineStyles from '../components/timeline/timeline.css';
 
 const TimelineTab = () => (
     <Box className={styles.wrapper}>
-        <Timeline />
+        <Timeline/>
+        <button className={`${timelineStyles.zoomButton} ${timelineStyles.zoomInButton}`}>
+            <img
+                draggable={false}
+                src={zoomIn}
+                alt="Zoom-In"
+            />
+        </button>
+        <button className={`${timelineStyles.zoomButton} ${timelineStyles.zoomOutButton}`}>
+            <img
+                draggable={false}
+                src={zoomOut}
+                alt="Zoom-Out"
+            />
+        </button>
     </Box>
 );
 
